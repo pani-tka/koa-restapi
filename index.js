4efeabf65d21c63c4165a9eb1f5fafa36dd98024
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2019-07-14 13:07:57 +0300

    Installed and implemented koa-cors

--- index.js
+++ index.js
@@ -1,59 +1,80 @@
 const Koa = require('koa');
 const Router = require('koa-router');
- 
+const cors = require('@koa/cors');
+
 const app = new Koa();
 const router = new Router();
+
+const koaOptions = {
+  origin: false,
+  credentials: false
+};
  
 router
  .param('title', (str, ctx, next) => {
    ctx.title = str;
    return next();
  })
+ //get all movies
+ .get('/movies', (ctx, next) => {
+  ctx.body = [
+    {
+      title: 'Casablanca',
+      year: 1942,
+      format: 'DVD',
+      actors: 'Humphrey Bogart'
+    },
+    {
+      title: 'Charade',
+      year: 1953,
+      format: 'DVD',
+      actors: 'Audrey Hepburn'
+    },
+    {
+      title: 'Cool Hand Luke',
+      year: 1967,
+      format: 'VHS',
+      actors: 'Paul Newman'
+    },
+    {
+      title: 'Blazing Saddles',
+      year: 1974,
+      format: 'VHS',
+      actors: 'Mel Brooks'
+    }
+
+  ];
+})
  //get a single movie by title of movie
  .get('/movies/:title', (ctx, next) => {
    ctx.body = {
-     title:  "lorem ipsum1hllhiuhp",
-     text: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elitconsectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-   }
+    title: 'Blazing Saddles',
+    year: 1974,
+    format: 'VHS',
+    actors: 'Mel Brooks'
+  }
  })
  //get movie by name of actor
  .get('/movies/:actorname', (ctx, next) => {
    ctx.body = [
-     {
-     title:  "lorem ipsum1",
-     text: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elitconsectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
+    {
+      title: 'Cool Hand Luke',
+      year: 1967,
+      format: 'VHS',
+      actors: 'Paul Newman'
     },
     {
-     title:  "lorem ipsum1",
-     text: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elitconsectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
+      title: 'Blazing Saddles',
+      year: 1974,
+      format: 'VHS',
+      actors: 'Mel Brooks'
     }
    ]
  })
- //get all movies
- .get('/movies', (ctx, next) => {
-   ctx.body = [
-     {
-       title:  "lorem ipsum1",
-       text: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elitconsectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-     },
-     {
-       title:  "lorem ipsum2",
-       text: "Lorem ipsum dolor sit amet  Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-     },
-     {
-       title:  "lorem ipsum3",
-       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-     },
-     {
-       title:  "lorem ipsum4",
-       text: "Lorem ipsum dolor sitLorem ipsum dolor sit amet consectetur adipisicing elit amet consectetur adipisicing elit. Pariatur fugit fuga ut voluptas nostrum ullam eum accusantium consequuntur necessitatibus dolores. Pariatur hic officiis numquam ipsam perspiciatis officia maxime iusto vitae."
-     }   
-   ];
- })
- 
- 
+
 app
+ .use(cors(koaOptions))
  .use(router.routes())
  .use(router.allowedMethods());
  
- const server = app.listen(8080);
+ app.listen(8080);
