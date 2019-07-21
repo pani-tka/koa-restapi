850ab1160dbcf69d947add3f38bf9a4056009649
Kateryna Tkachenko <katherinatkachenko@gmail.com>
2019-07-22 01:44:16 +0300

    Connected mongodb and current changes for routing

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const cors = require('@koa/cors');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+
+//
+// Connect to DB
+//
+const url = 'mongodb://localhost:27017';
+
+const dbName = 'moviesDB';
+const collectionName = 'movies';
+const client = new MongoClient(url);
+
+let collection = null;
+
+(async function() {
+  await client.connect();
+  col = client
+  .db(dbName)
+  .collection(collectionName);
+  console.log("Connected correctly to server");
+})();
 
 const app = new Koa();
 const router = new Router();
@@ -11,66 +32,43 @@ const koaOptions = {
 };
  
 router
- .param('title', (str, ctx, next) => {
-   ctx.title = str;
-   return next();
- })
- //get all movies
- .get('/movies', (ctx, next) => {
-  ctx.body = [
-    {
-      title: 'Casablanca',
-      year: 1942,
-      format: 'DVD',
-      actors: 'Humphrey Bogart'
-    },
-    {
-      title: 'Charade',
-      year: 1953,
-      format: 'DVD',
-      actors: 'Audrey Hepburn'
-    },
-    {
-      title: 'Cool Hand Luke',
-      year: 1967,
+ .get('/', (ctx, next) => {
+    ctx.body = 'Hello, Dmitrii';
+  })
+  //get single movie by title
+  .get('/movies/:title', async (ctx, next) => {
+    const singleMovie = await col.findOne({"title": (ctx.params.title)});
+    ctx.body = JSON.stringify(singleMovie, null, 2);
+  })
+  //det movie info
+  .get('/movies/:id', async (ctx, next) => {
+    const singleMovie = await col.findOne({"_id": ObjectID(ctx.params.id)});
+    ctx.body = JSON.stringify(singleMovie, null, 2);
+  })
+  //get movies by actor
+  .get('/movies/:actor', async (ctx,next) => {
+    const moviesByActor = await col.find({"actors": (ctx.params.actor)});
+    ctx.body = JSON.stringify(moviesByActor, null, 2);
+  })
+  //get movie list
+  .get('/movies', async (ctx, next) => {
+    const allMovies = await col.find({}).toArray();
+    ctx.body = JSON.stringify(allMovies, null, 2);
+  })
+  .post('/movies', async (ctx, next) => {
+    const newMovie = {
+      title: "I'm new movie",
+      year: 2010,
       format: 'VHS',
-      actors: 'Paul Newman'
-    },
-    {
-      title: 'Blazing Saddles',
-      year: 1974,
-      format: 'VHS',
-      actors: 'Mel Brooks'
-    }
+      actors: [
+        'Kianu Rivz',
+        'Scarlet Yohanson',
+      ],
+    };
 
-  ];
-})
- //get a single movie by title of movie
- .get('/movies/:title', (ctx, next) => {
-   ctx.body = {
-    title: 'Blazing Saddles',
-    year: 1974,
-    format: 'VHS',
-    actors: 'Mel Brooks'
-  }
- })
- //get movie by name of actor
- .get('/movies/:actorname', (ctx, next) => {
-   ctx.body = [
-    {
-      title: 'Cool Hand Luke',
-      year: 1967,
-      format: 'VHS',
-      actors: 'Paul Newman'
-    },
-    {
-      title: 'Blazing Saddles',
-      year: 1974,
-      format: 'VHS',
-      actors: 'Mel Brooks'
-    }
-   ]
- })
+    const insertResult = await col.insertOne(newMovie);
+    ctx.body = JSON.stringify(insertResult.ops[0], null, 2);
+  })
 
 app
  .use(cors(koaOptions))
